0ba3aea7b5046f845b6c011f239ac10d37659e1c
Juhan-Ahamed <juhan669@gmail.com>
2023-07-25 14:29:01 +0600

    update overall template for version 2.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,23 @@
 (function () {
+  /* ========= Preloader ======== */
+  const preloader = document.querySelectorAll('#preloader')
+
+  window.addEventListener('load', function () {
+    if (preloader.length) {
+      this.document.getElementById('preloader').style.display = 'none'
+    }
+  })
+
+  /* ========= Add Box Shadow in Header on Scroll ======== */
+  window.addEventListener('scroll', function () {
+    const header = document.querySelector('.header')
+    if (window.scrollY > 0) {
+      header.style.boxShadow = '0px 0px 30px 0px rgba(200, 208, 216, 0.30)'
+    } else {
+      header.style.boxShadow = 'none'
+    }
+  })
+
   /* ========= sidebar toggle ======== */
   const sidebarNavWrapper = document.querySelector(".sidebar-nav-wrapper");
   const mainWrapper = document.querySelector(".main-wrapper");
--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,24 @@
+const gulp = require("gulp");
+const { src, dest, watch, series } = require("gulp");
 
-const gulp = require('gulp');
-const { src, dest, watch, series } = require('gulp');
+const sass = require("gulp-sass")(require("sass"));
 
-const sass = require("gulp-sass")(require("sass"))
-
-const browserSync = require('browser-sync').create();
+const browserSync = require("browser-sync").create();
 
 // Sass Task
 function scssTask() {
-  return src('assets/scss/*.scss')
-  .pipe(sass())
-  .pipe(gulp.dest('assets/css/'))
+  return src("./assets/scss/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("./assets/css/"));
 }
 
-// BrowserSync Task 
+// BrowserSync Task
 function browserSyncServer(cb) {
   browserSync.init({
     server: {
-      baseDir: '.'
-    }
-  })
+      baseDir: ".",
+    },
+  });
   cb();
 }
 
@@ -30,13 +29,8 @@ function browserSyncReload(cb) {
 
 // Watch Task
 function watchTask() {
-  watch(['*.html', 'assets/js/**/*.js'], browserSyncReload);
-  watch(['assets/scss/**/*.scss'], series(scssTask, browserSyncReload));
+  watch(["*.html", "./assets/js/**/*.js"], browserSyncReload);
+  watch(["./assets/scss/**/*.scss"], series(scssTask, browserSyncReload));
 }
 
-
-exports.default = series (
-  scssTask,
-  browserSyncServer,
-  watchTask
-);
+exports.default = series(scssTask, browserSyncServer, watchTask);
